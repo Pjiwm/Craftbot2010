340a23609b72accfbc3eb82880697dedbfb010e9
Pjiwm <Pim.Munne@gmail.com>
2021-12-27 18:05:52 +0100

    commands get registered at startup

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,65 @@
-import DiscordJS, { Intents } from 'discord.js'
-
-
-const client = new DiscordJS.Client({
+declare module "discord.js" { export interface Client { commands: Collection<unknown, any> } }
+import { Intents, Interaction, Client, Collection } from 'discord.js'
+import fs from 'fs'
+import { REST } from '@discordjs/rest'
+import { Routes } from 'discord-api-types/v9'
+const TOKEN = process.env.DISCORD_KEY || '0'
+const TEST_GUILD_ID = ''
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
+const client = new Client({
     intents: [
         Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_MESSAGES,
         Intents.FLAGS.GUILD_MESSAGE_REACTIONS
     ]
 })
+
+// get our commands
+const commands: any[] = []
+client.commands = new Collection()
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`)
+    commands.push(command.data.toJSON())
+    client.commands.set(command.data.name, command)
+}
+
 // startup
 client.on('ready', () => {
     console.log('bot is online')
+    // registering the commands
+    const CLIENT_ID = client.user?.id || '0'
+    const rest = new REST({
+        version: '9'
+    }).setToken(TOKEN);
+
+    (async () => {
+        try {
+            if (!TEST_GUILD_ID) {
+                await rest.put(
+                    Routes.applicationCommands(CLIENT_ID), {
+                    body: commands
+                },
+                )
+                console.log('Successfully registered application commands globally')
+            } else {
+                await rest.put(
+                    Routes.applicationGuildCommands(CLIENT_ID, TEST_GUILD_ID), {
+                    body: commands
+                },
+                )
+                console.log('Successfully registered application commands for development guild')
+            }
+        } catch (error) {
+            if (error) console.error(error)
+        }
+    })()
+
+    // set activity and status
     client.user?.setActivity({
         name: 'with your mom',
         type: 'PLAYING'
     })
-
     client.user?.setStatus('dnd')
-})
-
-// Command manager
-const guildId = '925061126046220409'
-const guild = client.guilds.cache.get(guildId)
-let commands
-
-if (guild) {
-    commands = guild.commands
-} else {
-    commands = client.application?.commands
-}
-
-commands?.create({
-    name: 'ping',
-    description: 'response with pong'
 })
 
 client.login(process.env.DISCORD_KEY)
