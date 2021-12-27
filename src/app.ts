3651a82e8bb7813795704d15078a700418882dce
Pjiwm <Pim.Munne@gmail.com>
2021-12-27 17:46:36 +0100

    made command reader

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import DiscordJS, { Intents } from 'discord.js'
 
+
 const client = new DiscordJS.Client({
     intents: [
         Intents.FLAGS.GUILDS,
@@ -7,9 +8,31 @@ const client = new DiscordJS.Client({
         Intents.FLAGS.GUILD_MESSAGE_REACTIONS
     ]
 })
-
+// startup
 client.on('ready', () => {
     console.log('bot is online')
+    client.user?.setActivity({
+        name: 'with your mom',
+        type: 'PLAYING'
+    })
+
+    client.user?.setStatus('dnd')
+})
+
+// Command manager
+const guildId = '925061126046220409'
+const guild = client.guilds.cache.get(guildId)
+let commands
+
+if (guild) {
+    commands = guild.commands
+} else {
+    commands = client.application?.commands
+}
+
+commands?.create({
+    name: 'ping',
+    description: 'response with pong'
 })
 
 client.login(process.env.DISCORD_KEY)
