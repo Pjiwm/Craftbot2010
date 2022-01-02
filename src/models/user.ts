cb7303f33b9e4721527d07ca9aa03f354291ac6a
Pjiwm <Pim.Munne@gmail.com>
2022-01-02 18:42:08 +0100

    made mongoose user model

---
+++ src/models/user.ts
@@ -0,0 +1,25 @@
+import { Document, Schema, model } from "mongoose"
+
+export interface IUser extends Document {
+    userId: string,
+    positiveScoreCount: number
+    negativeScoreCount: number
+}
+
+const UserSchema = new Schema({
+    guildId: {
+        type: 'string',
+        required: true,
+        unique: true
+    },
+    positiveScoreCount: {
+        type: 'number',
+        default: 0
+    },
+    negativeScoreCount: {
+        type: 'number',
+        default: 0
+    }
+})
+
+export const ServerModel = model<IUser>('user', UserSchema)
