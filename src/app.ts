17217b1a470af4178f3fe4f898a44b86b7a9c242
Pjiwm <Pim.Munne@gmail.com>
2022-01-02 18:05:59 +0100

    isolated reactionAdd event

--- src/app.ts
+++ src/app.ts
@@ -27,7 +27,7 @@ for (const file of commandFiles) {
     commands.push(command.data)
     client.commands.set(command.data.name, command)
 }
-
+// register commands for development guild
 const rest = new REST({ version: '9' }).setToken(TOKEN);
 (async () => {
     try {
@@ -57,24 +57,5 @@ for (const file of eventFiles) {
         })
     }
 }
-
-client.on('messageReactionAdd', async (reaction, user) => {
-    // When a reaction is received, check if the structure is partial
-    if (reaction.partial) {
-        // If the message this reaction belongs to was removed, the fetching might result in an API error which should be handled
-        try {
-            await reaction.fetch()
-        } catch (error) {
-            console.error('Something went wrong when fetching the message:', error)
-            // Return as `reaction.message.author` may be undefined/null
-            return
-        }
-    }
-
-    // Now the message has been cached and is fully available
-    console.log(`${reaction.message.author}'s message "${reaction.message.content}" gained a reaction!`)
-    // The reaction is now also fully available and the properties will be reflected accurately:
-    console.log(`${reaction.count} user(s) have given the same reaction to this message!`)
-})
 
 client.login(process.env.DISCORD_KEY)
---
+++ src/events/message.reaction.add.ts
@@ -0,0 +1,41 @@
+import { Client, CommandInteraction, Interaction, MessageReaction, User } from "discord.js"
+
+export = {
+    name: 'messageReactionAdd',
+    async execute(messageReaction: MessageReaction, user: User) {
+        // When a reaction is received, check if the structure is partial
+        if (messageReaction.partial) {
+            // If the message this reaction belongs to was removed, the fetching might result in an API error which should be handled
+            try {
+                await messageReaction.fetch()
+            } catch (error) {
+                console.error('Something went wrong when fetching the message:', error)
+                // Return as `reaction.message.author` may be undefined/null
+                return
+            }
+        }
+
+        // Now the message has been cached and is fully available
+        console.log(`${messageReaction.message.author}'s message "${messageReaction.message.content}" gained a reaction!`)
+        // The reaction is now also fully available and the properties will be reflected accurately:
+        console.log(`${messageReaction.count} user(s) have given the same reaction to this message!`)
+    }
+}
+// client.on('messageReactionAdd', async (messageReaction, user) => {
+//     // When a reaction is received, check if the structure is partial
+//     if (messageReaction.partial) {
+//         // If the message this reaction belongs to was removed, the fetching might result in an API error which should be handled
+//         try {
+//             await messageReaction.fetch()
+//         } catch (error) {
+//             console.error('Something went wrong when fetching the message:', error)
+//             // Return as `reaction.message.author` may be undefined/null
+//             return
+//         }
+//     }
+
+//     // Now the message has been cached and is fully available
+//     console.log(`${reaction.message.author}'s message "${reaction.message.content}" gained a reaction!`)
+//     // The reaction is now also fully available and the properties will be reflected accurately:
+//     console.log(`${reaction.count} user(s) have given the same reaction to this message!`)
+// })
