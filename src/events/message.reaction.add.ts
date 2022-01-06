1e0dca7054f548fb0d0af1ee12c7ef70569cb9d4
Pjiwm <Pim.Munne@gmail.com>
2022-01-06 16:39:17 +0100

    correctly track score emojis on reactions

--- src/events/message.reaction.add.ts
+++ src/events/message.reaction.add.ts
@@ -19,19 +19,33 @@ export = {
         const messageAuthor = messageReaction.message.author
         const reactingUser = user
         const guildId = messageReaction.message.guildId
+        let isPositiveScore
+        let guild
+        let emoji
 
         // check if a reaction for tracking score is used, if the server is not stored yet we add it
         if (!await ServerModel.findOne({ guildId: guildId })) {
             await ServerModel.create({ guildId: guildId })
         }
 
-        const guild = await ServerModel.findOne({ guildId: guildId })
-        console.log(guild?.positiveScore)
+        guild = await ServerModel.findOne({ guildId: guildId })
 
-        // we don't wanna track bots
+        // check if it's the correct emoji 
+        emoji = messageReaction.emoji.name
+        if (emoji === guild?.positiveScore) {
+            isPositiveScore = true
+        } else if (emoji === guild?.negativeScore) {
+            isPositiveScore = false
+        } else {
+            // not the emoji we're tracking
+            return
+        }
+
+        // we don't want to track bots
         if (messageAuthor?.bot) {
             return
         }
+
         // if user is not present in DB add them first.
         if (!await UserModel.findOne({ userId: messageAuthor?.id })) {
             await UserModel.create({ userId: messageAuthor?.id })
@@ -47,10 +61,13 @@ export = {
             return
         }
 
-        UserModel.findOneAndUpdate({ userId: messageAuthor }, { $inc: { positiveScoreCount: 1 } })
-        // Now the message has been cached and is fully available
-        console.log(`${messageReaction.message.author?.id}'s message "${messageReaction.message.content}" gained a reaction!`)
-        // The reaction is now also fully available and the properties will be reflected accurately:
-        console.log(`${messageReaction.count} user(s) have given the same reaction to this message!`)
+        // update the correct score
+        if (isPositiveScore) {
+            UserModel.findOneAndUpdate({ userId: messageAuthor }, { $inc: { positiveScoreCount: 1 } })
+        } else {
+            UserModel.findOneAndUpdate({ userId: messageAuthor }, { $inc: { negativeScoreCount: 1 } })
+        }
+
+        console.log(`[${guildId}]: ${reactingUser.username} reacted to ${messageAuthor?.username} with '${emoji}'`)
     }
 }
